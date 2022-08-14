4daaacabae5b99284c84d804710fa64adccbac4f
Code4Daze <jevon.fillette@gmail.com>
2022-08-14 15:07:12 -0400

    Start working on sidebar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './css/style.css';
 import {createHeader, createSidebar, createFooter} from './initLoad';
 
-let test = "Hello World";
-console.log(test);
-
 document.body.appendChild(createHeader());
+document.body.appendChild(createSidebar());
--- src/initLoad.js
+++ src/initLoad.js
@@ -1,5 +1,6 @@
 import {addThemeButtons} from './theme';
 import Logo from './imgs/logo.svg';
+import Add from './imgs/add.svg';
 
 function createHeader() {
   let header = document.createElement('header');
@@ -30,7 +31,43 @@ function createHeader() {
 }
 
 function createSidebar() {
+  let sidebar = document.createElement('div');
+  sidebar.classList.add('sidebar');
 
+  let defaultTabs = document.createElement('ul');
+  defaultTabs.classList.add('default');
+
+  let homeLi = document.createElement('li');
+  homeLi.textContent = "Home";
+  let todayLi = document.createElement('li');
+  todayLi.textContent = "Today";
+  let weekLi = document.createElement('li');
+  weekLi.textContent = "This Week";
+
+  defaultTabs.appendChild(homeLi);
+  defaultTabs.appendChild(todayLi);
+  defaultTabs.appendChild(weekLi);
+
+  let projectsTabs = document.createElement('ul');
+  projectsTabs.classList.add('projectsList');
+  
+  let projects = document.createElement('li');
+  projects.classList.add('projects');
+
+  let project = document.createTextNode('Projects');
+  let add = document.createElement('img');
+  add.setAttribute('src', `${Add}`);
+  add.setAttribute('alt', "Add Project");
+
+  projects.appendChild(project);
+  projects.appendChild(add);
+
+  projectsTabs.appendChild(projects);
+
+  sidebar.appendChild(defaultTabs);
+  sidebar.appendChild(projectsTabs);
+
+  return sidebar;
 }
 
 function createFooter() {
